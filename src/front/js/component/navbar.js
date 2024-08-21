aab629ef553e976015b95d84fc1ea751f8eda149
emanueldelc <165728564+emanueldelc@users.noreply.github.com>
2024-08-21 23:05:52 +0000

    added LogIn page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,11 +9,14 @@ export const Navbar = () => {
 			<div className="container-fluid px-3">
 				<div className="logoDiv d-flex align-items-center">
 					<img id="BBLogo" src={BBLogo} alt="logo-img"></img>
-				<h1 className="ms-3">Brew Buddy</h1>
+					<h1 className="ms-3">BrewBuddy</h1>
 				</div>
 				<div className="ml-auto navbarButtons">
 					<button className="btn btn-dark me-3">Profile</button>
-					<button className="btn btn-dark">Log In</button>
+					<Link to="/login">
+						<button className="btn btn-dark">Log In</button>
+
+					</Link>
 				</div>
 			</div>
 			{/* <div className="container">
@@ -26,6 +29,18 @@ export const Navbar = () => {
 					</Link>
 				</div>
 			</div> */}
+			<ul className="nav nav-tabs mt-2 ps-3">
+				<li className="nav-item">
+					<a className="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">Favorites</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">Brews Near Me</a>
+				</li>
+			
+			</ul>
 		</nav>
 	);
 };
