deb2d1b584f9fdeaa47ae1c38cb5227786138181
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-08-22 00:47:31 +0000

    fixed the fetch function

--- src/front/js/component/BootstrapCard.js
+++ src/front/js/component/BootstrapCard.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const BreweryCard = (props) => {
@@ -8,11 +8,8 @@ export const BreweryCard = (props) => {
     const navigate = useNavigate()
     return (
         <div class="card">
-            <div class="card-header">
-                {props.brewery_data.name}
-            </div>
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
+                <h5 class="card-title">{props.breweryData.name}</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,17 +3,19 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import brewbuddyimg from "../../img/DALL·E 2024-08-18.png"
 import "../../styles/home.css";
+import { BreweryCard } from "../component/BootstrapCard";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-	const eachBrewery = store.brewery_data.map((brewery_data, index) => (
-		<BreweryCard key={index} brewery_data={brewery_data} />
+	const eachBrewery = store.breweryData.map((breweryData, index) => (
+		<BreweryCard key={index} breweryData={breweryData} />
 	))
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Brew Buddy!!</h1>
 			<p>
 				<img src={brewbuddyimg} />
+				{eachBrewery}
 			</p>
 			<button onClick={actions.fetchBreweryInfo}>Fetch Brewery Info</button>
 		</div>
