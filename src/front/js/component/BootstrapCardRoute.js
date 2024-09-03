a95923136239334ae7ac455e76c4db3e3e00fb8a
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-09-03 15:32:36 +0000

    updated bootstrapcardroute.js

--- src/front/js/component/BootstrapCardRoute.js
+++ src/front/js/component/BootstrapCardRoute.js
@@ -14,39 +14,47 @@ export const BreweryRouteCard = (props) => {
                 <h2>Brew Locator</h2>
                 <div className="row">
                     <div className="col-4 border border-solid">
-                        <input type="text" placeholder="Find Your Brew . . ."></input>
+                        <input type="text" placeholder="Find Your Brew . . . . ." className="wide-input"></input>
+                        <button><i className="fa-solid fa-magnifying-glass bg-secondary-subtle"></i></button>
                         <div>
-                            <ul className="vertical-menu">
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
-                                <li>list 1</li>
+                            <ul className="vertical-menu w-100">
+                                <li className="border border-solid">
+                                    <div>
+                                        Name
+                                        </div>
+                                    <div>
+                                        Address
+                                    </div>
+                                    <div>
+                                        Number
+                                    </div>
+                                </li>
+                                <li className="border border-solid">
+                                    <div>
+                                        Name
+                                        </div>
+                                    <div>
+                                        Address
+                                    </div>
+                                    <div>
+                                        Number
+                                    </div>
+                                </li>
+                                <li className="border border-solid">
+                                    <div>
+                                        Name
+                                        </div>
+                                    <div>
+                                        Address
+                                    </div>
+                                    <div>
+                                        Number
+                                    </div>
+                                </li>
                             </ul>
                         </div>
                     </div>
-                        {/* map side of brewery locator */}
+                    {/* map side of brewery locator */}
                     <div className="col-8 border border-solid">
                         <p>Map Side</p>
                     </div>
