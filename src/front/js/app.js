5ff29e74ffdf5d9f317f8f354e3b6aff2714572f
EQ <167933418+ejquast@users.noreply.github.com>
2024-09-06 22:32:13 +0000

    Started initial backend and minor frontend code for the image upload function

---
+++ src/front/js/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
+import { AdvancedImage } from '@cloudinary/react';
+
+const App = () => {
+  const cld = new Cloudinary({ cloud: { cloudName: 'dprmqr54a' } });
+  
+  // Use this sample image or upload your own via the Media Explorer
+  const img = cld
+        .image('cld-sample-5')
+        .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+
+  return (<AdvancedImage cldImg={img}/>);
+};
+
+export default App
