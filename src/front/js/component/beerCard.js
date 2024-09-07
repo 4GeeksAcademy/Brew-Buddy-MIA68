e789df9cdb77c4bff86bc10f7fe1b8eb2445c9e5
Ryan Castanier <167449198+Zioraan@users.noreply.github.com>
2024-09-07 15:57:01 +0000

    Created basic beer layout

--- src/front/js/component/beerCard.js
+++ src/front/js/component/beerCard.js
@@ -8,12 +8,31 @@ export const BeerCard = (props) => {
     const { store, actions } = useContext(Context);
 
     return (
-        <div className="card">
+        <div className="card col-sm-6 col-md-4 my-2">
             <div className="card-body">
-                <h4 className="card-title">
-                    {/* J.R.: A specific image for brewery type */}
-                    {props.beerData.beer_name}
-                </h4>
+                <div className="d-flex">    
+                    <h2 className="card-title mx-auto">
+                        {/* J.R.: A specific image for brewery type */}
+                        {props.beerData.beer_name}
+                    </h2>
+                </div>
+                <div>
+                    <div className="row justify-content-between bg-danger">
+                        <h5 className="col">Type:</h5>
+                        <h5 className="col">{props.beerData.type}</h5>
+                        
+                        
+                    </div>
+                    <div className="row justify-content-between bg-light">
+                        <h5 className="col">Flavor:</h5>
+                        <h5 className="col">{props.beerData.flavor}</h5>
+                        
+                    </div>
+                    <div className="row justify-content-between bg-secondary">
+                        <h5 className="col">ABV:</h5>
+                        <h5 className="col">{props.beerData.ABV}</h5>
+                    </div>
+                </div>
             </div>
         </div>
     )
