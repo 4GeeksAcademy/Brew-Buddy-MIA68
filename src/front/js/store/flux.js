5496a7d5b54da81a865059e19f06a5480b63b746
Ryan Castanier <167449198+Zioraan@users.noreply.github.com>
2024-09-09 08:53:56 +0000

    beers are now linked and have a basic informational page linked to breweries

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -413,6 +413,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 					const data = await response.json();
 					setStore({ beerData: data });
+					console.log(data)
 				}
 				catch (error) {
 					console.error("Error fetching beer info", error);
