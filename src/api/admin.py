6b6fb19fe502804e4f462ca2957f33a8626d2275
EQ <167933418+ejquast@users.noreply.github.com>
2024-09-10 00:37:09 +0000

    Updated fron-end code in flux and created new .js files

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Beer, Brewery, FavoriteBeers, FavoriteUsers, FavoriteBreweries, PointTransaction
+from .models import db, User, Beer, Brewery, FavoriteBeers, FavoriteUsers, FavoriteBreweries, PointTransaction, UserImage
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -18,7 +18,7 @@ def setup_admin(app):
     admin.add_view(ModelView(FavoriteUsers, db.session))
     admin.add_view(ModelView(FavoriteBreweries, db.session))
     admin.add_view(ModelView(PointTransaction, db.session))
-
+    admin.add_view(ModelView(UserImage, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
