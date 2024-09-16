4da981ea63d45f699f790767c1f308b071b87f97
EQ <167933418+ejquast@users.noreply.github.com>
2024-09-16 03:50:54 +0000

    Updates to backend (models and routes) and front end (flux, navbar, UserProfile) in order to fix the image upload function for profile images; still needs updating to work completely as only the default pic works

--- src/api/models.py
+++ src/api/models.py
@@ -225,14 +225,12 @@ class PointTransaction(db.Model):
 
 # model for user uploaded images
 class UserImage(db.Model):
-    __table_args__ = (
-        db.UniqueConstraint("owner_id", name="unique_img_title_user"),
-    )
+    __table_args__ = (db.UniqueConstraint('owner_id', 'is_profile_image', name='unique_profile_image_per_user'),)
     id = db.Column(db.Integer, primary_key=True)
     is_profile_image = db.Column(db.Boolean(), nullable=False, default=False)
     public_id = db.Column(db.String(500), nullable=False, unique=True)
     image_url = db.Column(db.String(500), nullable=False, unique=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     owner = db.relationship("User", back_populates="user_images", foreign_keys=[owner_id])
 
     def __init__(self, public_id, image_url, owner_id, is_profile_image=False):
