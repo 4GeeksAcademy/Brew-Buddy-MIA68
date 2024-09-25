bca6f131b319b2857dc598893db841dcff6a5d89
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-09-25 19:42:48 +0000

    fixed reviews again

--- src/front/js/pages/Search.js
+++ src/front/js/pages/Search.js
@@ -13,23 +13,27 @@ import background1 from "../../img/DALL·E 2024-09-04.webp"
 export const Search = () => {
 	const { store, actions } = useContext(Context);
 
+	useEffect(() => {
+		actions.getBreweryReviewsFromBackend();
+	}, []); // Empty array ensures it runs only once
+
 	const eachBrewery = store.breweryData.map((breweryData, index) => (
 		<BreweryCard key={index} breweryData={breweryData} />
 	))
 
 	return (
 		<div className="home-container">
-            <img src={background1} alt="Background" className="background-image" />
-            <div className="content">
+			<img src={background1} alt="Background" className="background-image" />
+			<div className="content">
 				<div className="text-center mt-5">
 					<img src={HomeLogo1} />
 					<div className="my-2">
 						< Modal />
-					<div className="container">
-						<div className="row">
-							{eachBrewery}
+						<div className="container">
+							<div className="row">
+								{eachBrewery}
+							</div>
 						</div>
-					</div>
 					</div>
 				</div>
 			</div>
